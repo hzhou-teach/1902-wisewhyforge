e6ac2d9393c00f61139323f353dc9e054a1b6afc
wisewhyforge <wisewhyforge@users.noreply.github.com>
2020-07-27 11:55:18 -0500

    Create 3SumClosest.java

---
+++ 3SumClosest.java
@@ -0,0 +1,33 @@
+/*
+Joseph Luo 
+
+Time: 5-10 minutes
+
+Difficulty: 2
+
+Brute Force Approach
+*/
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return 0;
+        int N = nums.length;
+        int lowSum = 0;
+        int minDifference = Integer.MAX_VALUE;
+        for(int i = 0; i < N; i++){
+            for(int j = i+1; j < N; j++){
+                for(int k = j + 1; k < N; k++){
+                    int sum = nums[i] + nums[j] + nums[k];
+                    int diff = Math.abs(sum-target);
+                    if(diff < minDifference){
+                        minDifference = diff;
+                        lowSum = sum;
+                    }
+                    
+                }
+            }
+        }
+        
+        return lowSum;
+    }
+}
