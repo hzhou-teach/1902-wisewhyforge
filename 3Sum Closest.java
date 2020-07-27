7a8aefb57a115464fada011ca9793e34a63eabe9
wisewhyforge <wisewhyforge@users.noreply.github.com>
2020-07-27 11:59:27 -0500

    Update 3Sum Closest.java

--- 3Sum Closest.java
+++ 3Sum Closest.java
@@ -1,6 +1,9 @@
 /*
 Joseph Luo 
 
+Runtime: 9 ms, faster than 37.31% of Java online submissions for 3Sum Closest.
+Memory Usage: 39.4 MB, less than 14.70% of Java online submissions for 3Sum Closest.
+
 Time: 5-10 minutes
 
 Difficulty: 2
@@ -14,7 +17,7 @@ class Solution {
         int N = nums.length;
         int lowSum = 0;
         int minDifference = Integer.MAX_VALUE;
-        for(int i = 0; i < N; i++){
+        for(int i = 0; i < N && minDifference != 0; i++){
             for(int j = i+1; j < N; j++){
                 for(int k = j + 1; k < N; k++){
                     int sum = nums[i] + nums[j] + nums[k];
